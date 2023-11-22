5bc8507709aa155191422ba9d788dbc0e87624c3
punitda <punitdama@gmail.com>
2023-11-22 17:48:49 +0530

    New Header component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,24 +4,27 @@ import { GitHubIcon } from "./icons/GithubIcon";
 
 export function Header() {
   return (
-    <header className="sticky top-0 z-50 flex flex-none flex-wrap items-center justify-between bg-white px-4 py-5 shadow-md shadow-slate-900/5 transition duration-500 sm:px-6 lg:px-8">
-      <div className="relative flex flex-grow basis-0 items-center">
-        <Link
-          to="/"
-          aria-label="Home page"
-          className="inline-flex items-center"
-        >
-          <Logo className="h-9 w-auto stroke-sky-300 fill-sky-300" />
-          <h2 className="ml-2 text-2xl text-slate-700  hidden lg:block">
-            JCMPREPORT
-          </h2>
-        </Link>
-        <div className="relative flex basis-0 justify-end gap-6 sm:gap-8 md:flex-grow">
-          <Link to="https://github.com" className="group" aria-label="GitHub">
-            <GitHubIcon className="h-6 w-6 fill-slate-400 group-hover:fill-slate-500" />
+    <header className="absolute inset-x-0 top-0 z-50">
+      <nav
+        className="flex items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="flex lg:flex-1">
+          <Link to="/" className="-m-1.5 p-1.5">
+            <span className="sr-only">Home</span>
+            <Logo />
           </Link>
         </div>
-      </div>
+
+        <div className="flex flex-1 justify-end group">
+          <a
+            href="https://github.com/punitda/japicmp-frontend"
+            className="text-sm font-semibold leading-6 text-gray-900"
+          >
+            <GitHubIcon className="h-6 w-6 fill-slate-400 group-hover:fill-slate-500 dark:group-hover:fill-slate-300" />
+          </a>
+        </div>
+      </nav>
     </header>
   );
 }
