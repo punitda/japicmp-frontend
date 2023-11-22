64319e51baf8a104bb5d0a2265c401f5d5422d85
punitda <punitdama@gmail.com>
2023-11-22 17:49:10 +0530

    Update index page to use newly created components

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,8 +5,9 @@ import {
 } from "@remix-run/node";
 import { useFetcher } from "@remix-run/react";
 
-import { Background } from "~/components/Background";
 import { Button } from "~/components/Button";
+import { Header } from "~/components/Header";
+import { Hero } from "~/components/Hero";
 
 export const meta: MetaFunction = () => {
   return [
@@ -52,66 +53,82 @@ export default function Index() {
   const isSubmitting = fetcher.state === "submitting";
 
   return (
-    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 ">
-      <Background />
-      <div className="py-16 sm:px-2 lg:relative lg:px-0 lg:py-20">
-        <p className="inline bg-gradient-to-r from-indigo-200 via-sky-400 to-indigo-200 bg-clip-text font-display text-5xl tracking-tight text-transparent">
-          Generate source and binary compatibility report
-        </p>
-        <p className="mt-3 text-2xl tracking-tight text-slate-400">
-          Check compatibility, so you can update your libraries without worrying
-          about breaking the app
-        </p>
-        <fetcher.Form method="post" className="mt-12">
-          <label
-            htmlFor="old-library"
-            className="block text-md font-medium leading-6 text-gray-100"
-          >
-            Old Library
-          </label>
-          <div className="mt-2">
-            <input
-              type="text"
-              name="old-library"
-              id="old-library"
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
-              placeholder="<groupId>:<artifactId>:<version>"
-              aria-describedby="package name"
-            />
+    <div className="flex w-full flex-col bg-white">
+      <Header />
+      <main className="isolate">
+        <Hero />
+        <section
+          id="generate"
+          className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow-lg lg:w-1/2 w-4/5 mx-auto mt-6 mb-20"
+        >
+          <div className="px-4 py-5 sm:px-6">
+            <p className="text-gray-900 text-2xl font-semibold">
+              Generate Report
+            </p>
           </div>
-          <p className="inline-flex items-center rounded-md bg-pink-400/10 px-2 py-1 mt-1.5 text-xs font-medium text-pink-400 ring-1 ring-inset ring-pink-400/20">
-            Ex: com.squareup.okhttp3:okhttp:4.11.0
-          </p>
-          <label
-            htmlFor="new-library"
-            className="block text-md font-medium leading-6 text-gray-100 mt-3"
-          >
-            New Library
-          </label>
-          <div className="mt-2">
-            <input
-              type="text"
-              name="new-library"
-              id="new-library"
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
-              placeholder="<groupId>:<artifactId>:<version>"
-              aria-describedby="package name"
-            />
-          </div>
-          <p className="inline-flex items-center rounded-md bg-pink-400/10 px-2 py-1 mt-1.5 text-xs font-medium text-pink-400 ring-1 ring-inset ring-pink-400/20">
-            Ex: com.squareup.okhttp3:okhttp:4.12.0
-          </p>{" "}
-          <div className="mt-8 flex gap-4 md:justify-center lg:justify-start">
-            <Button type="submit">
-              {isSubmitting ? "Generating Report" : "Generate Report"}
-            </Button>
-          </div>
-        </fetcher.Form>
-      </div>
+          <fetcher.Form method="post" className="px-4 py-5 sm:p-6">
+            <label
+              htmlFor="old-library"
+              className="block text-md font-medium leading-6 text-gray-900"
+            >
+              Old Library
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="old-library"
+                id="old-library"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                placeholder="<groupId>:<artifactId>:<version>"
+                aria-describedby="package name"
+              />
+            </div>
+            <p className="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 mt-1.5 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
+              Ex: com.squareup.okhttp3:okhttp:4.11.0
+            </p>
+            <label
+              htmlFor="new-library"
+              className="block text-md font-medium leading-6 text-gray-900 mt-3"
+            >
+              New Library
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="new-library"
+                id="new-library"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                placeholder="<groupId>:<artifactId>:<version>"
+                aria-describedby="package name"
+              />
+            </div>
+            <p className="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 mt-1.5 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
+              Ex: com.squareup.okhttp3:okhttp:4.12.0
+            </p>{" "}
+            <div className="py-4">
+              <Button type="submit">
+                {isSubmitting ? "Generating Report" : "Generate Report"}
+              </Button>
+            </div>
+          </fetcher.Form>
+        </section>
 
-      {reportOutput ? (
-        <div dangerouslySetInnerHTML={{ __html: reportOutput }} />
-      ) : null}
+        {reportOutput ? (
+          <section
+            id="report-output"
+            className="mx-auto w-11/12 py-12 divide-y divide-gray-200 overflow-auto rounded-lg bg-white shadow-lg"
+          >
+            <div className="px-4 py-5 sm:px-6">
+              <p className="text-gray-900 text-2xl font-semibold">Report</p>
+            </div>
+
+            <div
+              dangerouslySetInnerHTML={{ __html: reportOutput }}
+              className="px-4 py-5 sm:p-6"
+            />
+          </section>
+        ) : null}
+      </main>
     </div>
   );
 }
