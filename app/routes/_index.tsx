bfc1b07e2383335fc21da91f6c2112011fb9d8c3
punitda <punitdama@gmail.com>
2023-11-24 11:40:55 +0530

    Use newly created components

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,18 +1,21 @@
-import type { ActionFunctionArgs, MetaFunction } from "@remix-run/node";
-import { json } from "@remix-run/node";
-import { useFetcher, useNavigate } from "@remix-run/react";
-
 import clsx from "clsx";
 import { useEffect, useState } from "react";
 
-import { Button } from "~/components/Button";
-import { Header } from "~/components/Header";
-import { Hero } from "~/components/Hero";
-
+import { json } from "@remix-run/node";
+import { useFetcher, useNavigate } from "@remix-run/react";
 import {
   generateReportUsingFile,
   generateReportUsingMaven,
 } from "~/services/report-generator.server";
+
+import { GenerateReportMavenForm } from "~/components/forms/GenerateReportMavenForm";
+import { GenerateReportFileForm } from "~/components/forms/GenerateReportFileForm";
+import { Header } from "~/components/Header";
+import { Hero } from "~/components/Hero";
+
+import type { ActionFunctionArgs, MetaFunction } from "@remix-run/node";
+import type { ReportFormData } from "~/types/ReportFormData";
+import { ReportOutput } from "~/components/ReportOutput";
 
 export const meta: MetaFunction = () => {
   return [
@@ -35,11 +38,6 @@ export async function action({ request }: ActionFunctionArgs) {
   return json({ error: "Unknown request", status: 400 });
 }
 
-interface FormData {
-  reportOutput?: string;
-  error?: string;
-}
-
 interface Tab {
   name: "Maven" | "File";
   current: boolean;
@@ -47,7 +45,7 @@ interface Tab {
 
 export default function Index() {
   const fetcher = useFetcher();
-  const data = fetcher.data as FormData;
+  const data = fetcher.data as ReportFormData;
   const isSubmitting = fetcher.state === "submitting";
   const navigate = useNavigate();
 
@@ -139,166 +137,24 @@ export default function Index() {
             </div>
           </div>
           {currentTab?.name == "Maven" ? (
-            <fetcher.Form method="post" className="px-4 py-5 sm:p-6">
-              <input type="hidden" name="reportType" value="Maven" />
-              <label
-                htmlFor="old-library"
-                className="block text-md font-medium leading-6 text-gray-900"
-              >
-                Old Library
-              </label>
-              <div className="mt-2">
-                <input
-                  type="text"
-                  name="old-library"
-                  id="old-library"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
-                  placeholder="<groupId>:<artifactId>:<version>"
-                  aria-describedby="package name"
-                />
-              </div>
-              <p className="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 mt-1.5 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
-                Ex: com.squareup.okhttp3:okhttp:4.11.0
-              </p>
-              <label
-                htmlFor="new-library"
-                className="block text-md font-medium leading-6 text-gray-900 mt-3"
-              >
-                New Library
-              </label>
-              <div className="mt-2">
-                <input
-                  type="text"
-                  name="new-library"
-                  id="new-library"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
-                  placeholder="<groupId>:<artifactId>:<version>"
-                  aria-describedby="package name"
-                />
-              </div>
-              <p className="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 mt-1.5 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
-                Ex: com.squareup.okhttp3:okhttp:4.12.0
-              </p>{" "}
-              <div className="py-4">
-                <Button type="submit">
-                  {isSubmitting ? "Generating Report" : "Generate Report"}
-                </Button>
-                {data?.error ? (
-                  <p className="text-red-400 mt-2">{data.error}</p>
-                ) : null}
-              </div>
-            </fetcher.Form>
+            <GenerateReportMavenForm
+              fetcher={fetcher}
+              isSubmitting={isSubmitting}
+              data={data}
+            />
           ) : null}
 
           {currentTab?.name == "File" ? (
-            <fetcher.Form
-              method="post"
-              className="px-4 py-5 sm:p-6"
-              encType="multipart/form-data"
-            >
-              <input type="hidden" name="reportType" value="File" />
-              <label
-                htmlFor="old-library-file"
-                className="block text-md font-medium leading-6 text-gray-900"
-              >
-                Old Library File
-              </label>
-              <div className="mt-2">
-                <input
-                  type="file"
-                  name="old-artifact"
-                  accept=".aar, .jar"
-                  onChange={(e) => {
-                    e.preventDefault();
-                  }}
-                />
-                <p className="text-xs leading-5 text-gray-400 mt-1">
-                  Only .aar and .jar files are allowed
-                </p>
-              </div>
-              <label
-                htmlFor="old-library-version"
-                className="block text-md font-medium leading-6 text-gray-900 mt-3"
-              >
-                Old Library Version
-              </label>
-              <div className="mt-2">
-                <input
-                  type="text"
-                  name="old-library-version"
-                  id="old-library-version"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
-                  placeholder="<version>"
-                  aria-describedby="version"
-                />
-              </div>
-              <p className="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 mt-1.5 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
-                Ex: 1.2.0
-              </p>
-              <label
-                htmlFor="new-library"
-                className="block text-md font-medium leading-6 text-gray-900 mt-3"
-              >
-                New Library File
-              </label>
-              <div className="mt-2">
-                <input
-                  type="file"
-                  name="new-artifact"
-                  accept=".aar, .jar"
-                  onChange={(e) => {
-                    e.preventDefault();
-                  }}
-                />
-                <p className="text-xs leading-5 text-gray-400 mt-1">
-                  Only .aar and .jar files are allowed
-                </p>
-              </div>
-              <label
-                htmlFor="new-library-version"
-                className="block text-md font-medium leading-6 text-gray-900"
-              >
-                New Library Version
-              </label>
-              <div className="mt-2">
-                <input
-                  type="text"
-                  name="new-library-version"
-                  id="new-library-version"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
-                  placeholder="<version>"
-                  aria-describedby="version"
-                />
-              </div>
-              <p className="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 mt-1.5 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
-                Ex: 1.2.2
-              </p>
-              <div className="py-4">
-                <Button type="submit">
-                  {isSubmitting ? "Generating Report" : "Generate Report"}
-                </Button>
-                {data?.error ? (
-                  <p className="text-red-400 mt-2">{data.error}</p>
-                ) : null}
-              </div>
-            </fetcher.Form>
+            <GenerateReportFileForm
+              fetcher={fetcher}
+              isSubmitting={isSubmitting}
+              data={data}
+            />
           ) : null}
         </section>
 
         {data?.reportOutput ? (
-          <section
-            id="report-output"
-            className="mx-auto w-11/12 py-12 divide-y divide-gray-200 overflow-auto rounded-lg bg-white shadow-lg"
-          >
-            <div className="px-4 py-5 sm:px-6">
-              <p className="text-gray-900 text-2xl font-semibold">Report</p>
-            </div>
-
-            <div
-              dangerouslySetInnerHTML={{ __html: data?.reportOutput }}
-              className="px-4 py-5 sm:p-6"
-            />
-          </section>
+          <ReportOutput reportOutput={data.reportOutput} />
         ) : null}
       </main>
     </div>
