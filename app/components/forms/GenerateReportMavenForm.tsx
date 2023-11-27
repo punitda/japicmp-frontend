f781074ce012713dfa044105c3331b69e88480b3
punitda <punitdama@gmail.com>
2023-11-27 11:13:27 +0530

    Decrease form input width

--- app/components/forms/GenerateReportMavenForm.tsx
+++ app/components/forms/GenerateReportMavenForm.tsx
@@ -33,7 +33,7 @@ export function GenerateReportMavenForm({
           type="text"
           name="new-library"
           id="new-library"
-          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+          className="block w-3/4 sm:w-1/2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
           placeholder="<groupId>:<artifactId>:<version>"
           aria-describedby="package name"
         />
@@ -53,7 +53,7 @@ export function GenerateReportMavenForm({
           type="text"
           name="old-library"
           id="old-library"
-          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+          className="block w-3/4 sm:w-1/2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
           placeholder="<groupId>:<artifactId>:<version>"
           aria-describedby="package name"
         />
